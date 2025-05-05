8939ddd110d2382e9711507fb782abaf49b6587e
maji <au860715@gmail.com>
2025-05-05 11:25:06 +0800

    [fix]deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,23 +2,26 @@
 
 running_containers=$(docker ps -q)
 
-# 檢查傳入的參數
-if [ "$1" == "stop" ]; then
+case "$1" in
+stop)
   docker-compose stop
-elif [ "$1" == "restart" ]; then
+  ;;
+restart)
   if [ -n "$running_containers" ]; then
     docker-compose stop
     docker-compose start
   else
     docker-compose start
   fi
-elif [ "$1" == "rebuild" ]; then
+  ;;
+rebuild)
   if [ -n "$running_containers" ]; then
     docker-compose down
   fi
   docker-compose up -d --build
   docker image prune -f
-else
-  # 如果沒有提供正確的參數，顯示使用方式
-  echo "請使用正確參數。"
-fi
+  ;;
+*)
+  echo "請使用正確參數。可用參數：stop | restart | rebuild"
+  ;;
+esac
